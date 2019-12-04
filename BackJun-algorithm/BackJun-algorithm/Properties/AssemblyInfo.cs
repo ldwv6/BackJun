5eb7d9e9f7f68e619f3c5a00cf4b1331026e4dfd
이등원 <ldwv6@interpark.com>
2019-12-04 11:20:07 +0900

    1008-201912041119

--- BackJun-algorithm/BackJun-algorithm/Properties/AssemblyInfo.cs
+++ BackJun-algorithm/BackJun-algorithm/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 어셈블리에 대한 일반 정보는 다음 특성 집합을 통해 
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
 // 이러한 특성 값을 변경하세요.
-[assembly: AssemblyTitle("BackJun")]
+[assembly: AssemblyTitle("BackJun-algorithm")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BackJun")]
+[assembly: AssemblyProduct("BackJun-algorithm")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 이 프로젝트가 COM에 노출되는 경우 다음 GUID는 typelib의 ID를 나타냅니다.
-[assembly: Guid("4c17428e-9282-4738-a4c8-abbec795d217")]
+[assembly: Guid("cf9f9865-fdaf-4fd5-a5f3-31323fbe2bd0")]
 
 // 어셈블리의 버전 정보는 다음 네 가지 값으로 구성됩니다.
 //
