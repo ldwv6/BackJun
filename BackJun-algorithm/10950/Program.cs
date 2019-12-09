4b0e3b86f775b05e9c0bb06c50578bdab3a8c086
이등원 <ldwv6@interpark.com>
2019-12-09 15:27:57 +0900

    201912090327

--- BackJun-algorithm/10950/Program.cs
+++ BackJun-algorithm/10950/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _10950
 {
@@ -10,6 +6,26 @@ namespace _10950
     {
         static void Main(string[] args)
         {
+            string[] s = Console.ReadLine().Split();;   
+            int numLine = int.Parse(s[0]);
+
+            string[] s1 = new string[numLine];
+            string[] s2 = new string[2];
+
+            for (int i=0;i< numLine ; i++)
+            {
+                s1[i] = Console.ReadLine();
+            }
+
+            for (int j=0;j< numLine; j++)
+            {
+                s2 = s1[j].Split(' ');
+
+                int a = int.Parse(s2[0]);
+                int b = int.Parse(s2[1]);
+
+                Console.WriteLine(a + b);
+            }
         }
     }
 }
