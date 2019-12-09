f7172d50b10fba7335df24aef12d195dd23ac245
이등원 <ldwv6@interpark.com>
2019-12-09 16:11:08 +0900

    201912090411

---
+++ BackJun-algorithm/8393/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _8393
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int numA = int.Parse(s);
+            int sum = 0;
+            for (int i = 1; i <= numA; i++)
+            {
+                sum += i;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
