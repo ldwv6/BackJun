3bffa01e7db31fc23d8e1fca6f303b10a5cea16e
이등원 <ldwv6@interpark.com>
2019-12-11 11:12:16 +0900

    1912111112

---
+++ BackJun-algorithm/11022/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int numA = int.Parse(s);
+            StringBuilder addNum = new StringBuilder();
+
+            for (int i = 0; i < numA; i++)
+            {
+                string[] s1 = Console.ReadLine().Split();
+
+                int a = int.Parse(s1[0]);
+                int b = int.Parse(s1[1]);
+
+                int result = a + b;
+                addNum.Append("Case #" + (i + 1) + ": " + a + " + " + b +  result + "\n");
+            }
+
+            Console.WriteLine(addNum.ToString());
+        }
+    }
+}
---
+++ BackJun-algorithm/2438/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _2438
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int numA = int.Parse(s);
+
+            for (int i = 0; i < numA; i++)
+            {
+                for(int j=0;j<i+1;j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
