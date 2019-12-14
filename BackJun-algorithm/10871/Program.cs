69833020eee1a6a11a797702295898cdb3198a9b
LOGAN LEE <ldwv6@naver.com>
2019-12-14 21:37:39 +0900

    201912140937

---
+++ BackJun-algorithm/10871/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _10871
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split();
+            string[] s1 = Console.ReadLine().Split();
+            int[] intArray = new int[10];
+
+            int firstNum = int.Parse(s[0]);
+            int secondNum = int.Parse(s[1]);
+         
+            for (int i = 0;i<firstNum ;i++)
+            {
+                intArray[i] = int.Parse(s1[i]);
+
+                if(intArray[i] < secondNum)
+                {
+                    Console.Write(intArray[i] + " ");
+                }
+            }
+
+
+        }
+    }
+}
