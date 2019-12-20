25df25979b91aa4bd89c38555bbc7ef3b3710a47
이등원 <ldwv6@interpark.com>
2019-12-20 09:50:06 +0900

    201912200950

---
+++ BackJun-algorithm/10951/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+namespace _10951
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StringBuilder stringArray = new StringBuilder();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == null)
+                {
+                    break;
+                }
+
+
+                string[] s = input.Split();
+                int firstNum = int.Parse(s[0]);
+                int secondNum = int.Parse(s[1]);
+
+                Console.WriteLine(firstNum + secondNum);
+                //Console.Write(stringArray.ToString());
+                //stringArray.Append(firstNum + secondNum + "\n");
+            }
+      
+        }
+    }
+}
---
+++ BackJun-algorithm/1110/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _1110
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int i = 1;
+
+            int originNum = int.Parse(s);
+
+            while (true)
+            {
+                if (int.Parse(s) < 10)
+                {
+                    s = s + 0;
+                }
+
+                char[] s1 = s.ToCharArray();
+
+                int firstNum = int.Parse(s1[0].ToString());
+                int secondNum = int.Parse(s1[1].ToString());
+                int sum = firstNum + secondNum;
+                string s3 = secondNum.ToString();
+
+                string sumString = sum.ToString();
+             
+                string therLastNum = (sumString[sumString.Length - 1]).ToString();
+
+                string result = secondNum + therLastNum;
+
+                int result2 = int.Parse(result);
+
+                if (result2 == originNum)
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+
+                s = result2.ToString();
+
+                i++;
+            }
+
+        }
+    }
+}
