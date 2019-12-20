215eafdc7b7b962cd4c9b0710968325d67ea88de
이등원 <ldwv6@interpark.com>
2019-12-20 10:29:23 +0900

    201912201029

--- BackJun-algorithm/1110/Program.cs
+++ BackJun-algorithm/1110/Program.cs
@@ -11,36 +11,31 @@ namespace _1110
 
             int originNum = int.Parse(s);
 
+            if (int.Parse(s) < 10)
+            {
+                s = s + 0;
+            }
+
             while (true)
             {
-                if (int.Parse(s) < 10)
-                {
-                    s = s + 0;
-                }
-
                 char[] s1 = s.ToCharArray();
 
                 int firstNum = int.Parse(s1[0].ToString());
                 int secondNum = int.Parse(s1[1].ToString());
                 int sum = firstNum + secondNum;
+
                 string s3 = secondNum.ToString();
 
                 string sumString = sum.ToString();
-             
                 string therLastNum = (sumString[sumString.Length - 1]).ToString();
-
-                string result = secondNum + therLastNum;
-
-                int result2 = int.Parse(result);
+                s = secondNum + therLastNum;
+                int result2 = int.Parse(s);
 
                 if (result2 == originNum)
                 {
                     Console.WriteLine(i);
                     break;
                 }
-
-                s = result2.ToString();
-
                 i++;
             }
 
