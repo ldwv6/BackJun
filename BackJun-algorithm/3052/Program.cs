ea843a3e88aea70a87302965e6f2d8c502e557bb
이등원 <ldwv6@interpark.com>
2020-02-03 08:51:40 +0900

    202002030851

---
+++ BackJun-algorithm/3052/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _3052
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = new int[10];
+
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                intArray[i] = (int.Parse(Console.ReadLine())) % 42;
+            }
+
+            Console.WriteLine(intArray.Distinct().Count());
+        }
+    }
+}
